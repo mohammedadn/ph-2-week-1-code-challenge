4aa935ffd92f77217289b90f12d444e2fcfd1b87
Mohammedadn <mohammedaden023@gmail.com>
2024-04-29 20:17:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import { useState } from 'react';
 
 function App() {
   const transaction = [
-    {
-    date: "27/04/2024",
+      {
+      date: "27/04/2024",
     description: "Super Metro",
-    category: "Expenditure",
-    amount: "Ksh 900"
-    },
+      category: "Expenditure",
+     amount: "Ksh 980"
+     },
     {
       date: "28/04/2024",
         description: "Miscellaneous",
